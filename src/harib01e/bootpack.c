d2a4e576af45cc2c0d20abf6b77f671624461397
rhoboro <rhoboro@gmail.com>
2021-05-08 10:38:57 +0900

    harib01eを追加

---
+++ src/harib01e/bootpack.c
@@ -0,0 +1,16 @@
+void io_hlt(void);
+void write_mem8(int addr, int data);
+
+void HariMain(void) {
+  int i;
+  char *p = (char *)0xa0000;
+
+  for (i = 0; i <= 0xaffff; i++) {
+    p[i] = i & 0x0f;
+  }
+
+  for (;;) {
+    // naskfunc.nas の _io_hlt() を実行
+    io_hlt();
+  }
+}
