d8ba68518bee12a89e8cf43b5ecf92b2396b2c4d
rhoboro <rhoboro@gmail.com>
2021-05-10 21:08:33 +0900

    コメントを追加

--- src/harib05d/bootpack.c
+++ src/harib05d/bootpack.c
@@ -25,6 +25,7 @@ int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat);
 void HariMain(void) {
   struct BOOTINFO *binfo = (struct BOOTINFO *)ADR_BOOTINFO;
   char s[40], mcursor[256], keybuf[32], mousebuf[128];
+  // マウスの座標。左上が(mx, my) = (0, 0)
   int mx, my, i;
   struct MOUSE_DEC mdec;
 
@@ -90,18 +91,22 @@ void HariMain(void) {
 
           // 画面外にはみ出ないように調整
           if (mx < 0) {
+            // 左端
             mx = 0;
           }
           if (my < 0) {
+            // 上端
             my = 0;
           }
           if (mx > binfo->scrnx - 16) {
+            // 右端
             mx = binfo->scrnx - 16;
           }
           if (my > binfo->scrny - 16) {
+            // 下端
             my = binfo->scrny - 16;
           }
-          
+
           // 座標の表示を更新
           sprintf(s, "(%3d, %3d)", mx, my);
           boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 0, 0, 79, 15);
