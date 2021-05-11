94da421e8622e88cb85740a9934f00d89fc9405d
rhoboro <rhoboro@gmail.com>
2021-05-11 20:03:22 +0900

    harib06aを追加

---
+++ src/harib06a/bootpack.c
@@ -0,0 +1,106 @@
+/* bootpackのメイン */
+
+#include "bootpack.h"
+#include <stdio.h>
+
+extern struct FIFO8 keyfifo;
+extern struct FIFO8 mousefifo;
+
+void HariMain(void) {
+  struct BOOTINFO *binfo = (struct BOOTINFO *)ADR_BOOTINFO;
+  char s[40], mcursor[256], keybuf[32], mousebuf[128];
+  // マウスの座標。左上が(mx, my) = (0, 0)
+  int mx, my, i;
+  struct MOUSE_DEC mdec;
+
+  init_gdtidt();
+  init_pic();
+  io_sti();
+  fifo8_init(&keyfifo, 32, keybuf);
+  fifo8_init(&mousefifo, 128, mousebuf);
+  io_out8(PIC0_IMR, 0xf9); // PIC1とキーボードを許可(11111001)
+  io_out8(PIC1_IMR, 0xef); // マウスを許可(11101111)
+
+  init_keyboard();
+
+  init_palette();
+  init_screen8(binfo->vram, binfo->scrnx, binfo->scrny);
+  mx = (binfo->scrnx - 16) / 2; /* 画面中央になるように座標計算 */
+  my = (binfo->scrny - 28 - 16) / 2;
+  init_mouse_cursor8(mcursor, COL8_008484);
+  putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16);
+  sprintf(s, "(%d, %d)", mx, my);
+  putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
+
+  enable_mouse(&mdec);
+
+  for (;;) {
+    io_cli();
+    if (fifo8_status(&keyfifo) + fifo8_status(&mousefifo) == 0) {
+      io_stihlt();
+    } else {
+      if (fifo8_status(&keyfifo) != 0) {
+        i = fifo8_get(&keyfifo);
+        io_sti();
+        sprintf(s, "%02X", i);
+        boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 0, 16, 15, 31);
+        putfonts8_asc(binfo->vram, binfo->scrnx, 0, 16, COL8_FFFFFF, s);
+      } else if (fifo8_status(&mousefifo) != 0) {
+        // マウスのデータは3バイトくる
+        i = fifo8_get(&mousefifo);
+        io_sti();
+        if (mouse_decode(&mdec, i) != 0) {
+          // データが揃ったので処理を実行
+          sprintf(s, "[lcr %4d %4d]", mdec.x, mdec.y);
+          if ((mdec.btn & 0x01) != 0) {
+            // 1桁目が1ならクリック中で表示の l -> L にする
+            s[1] = 'L';
+          }
+          if ((mdec.btn & 0x02) != 0) {
+            // 2桁目が1なら右クリック中で表示の r -> R にする
+            s[3] = 'R';
+          }
+          if ((mdec.btn & 0x04) != 0) {
+            s[2] = 'C';
+          }
+          boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 32, 16,
+                   32 + 15 * 8 - 1, 31);
+          putfonts8_asc(binfo->vram, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
+
+          // マウスを動かす
+          // マウスを消す
+          boxfill8(binfo->vram, binfo->scrnx, COL8_008484, mx, my, mx + 15,
+                   my + 15);
+          mx += mdec.x;
+          my += mdec.y;
+
+          // 画面外にはみ出ないように調整
+          if (mx < 0) {
+            // 左端
+            mx = 0;
+          }
+          if (my < 0) {
+            // 上端
+            my = 0;
+          }
+          if (mx > binfo->scrnx - 16) {
+            // 右端
+            mx = binfo->scrnx - 16;
+          }
+          if (my > binfo->scrny - 16) {
+            // 下端
+            my = binfo->scrny - 16;
+          }
+
+          // 座標の表示を更新
+          sprintf(s, "(%3d, %3d)", mx, my);
+          boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 0, 0, 79, 15);
+          putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
+
+          // マウスを描く
+          putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16);
+        }
+      }
+    }
+  }
+}
